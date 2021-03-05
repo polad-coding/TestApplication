a1670c9065bb0e30a1982d8a0cabbacab7828878
Poliniko <polad20178@gmail.com>
2021-03-05 21:41:10 +0200

    Finished resposive design for mobile devices.

--- ClientApp/src/app/navigation-bar/navigation-bar.component.ts
+++ ClientApp/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.css']
+  styleUrls: ['./navigation-bar.component.css'],
+  host: {
+    '(document:click)': 'DocumentClicked()',
+  }
 })
-export class NavigationBarComponent implements OnInit {
+export class NavigationBarComponent implements OnInit, AfterViewInit {
+
+  public isMobile: boolean = false;
+  public actionsSideBarIsVisible: boolean = false;
+  public languagesSideBarIsVisible: boolean = false;
 
   constructor() { }
 
+  ngAfterViewInit(): void {
+    if (window.innerWidth < 768) {
+      this.isMobile = true;
+    }
+    else {
+      this.isMobile = false;
+    }
+  }
+
   ngOnInit() {
   }
+
+  public DocumentClicked() {
+    this.actionsSideBarIsVisible = false;
+    this.languagesSideBarIsVisible = false;
+  }
+
+  public DisplayActionsSideBar(event: MouseEvent)
+  {
+    event.stopPropagation();
+    this.actionsSideBarIsVisible = true;
+  }
+
+  public DisplayLanguagesSideBar(event: MouseEvent) {
+    event.stopPropagation();
+    this.languagesSideBarIsVisible = true;
+  }
+
+  public LanguagesSideBarClick(event: MouseEvent) {
+    event.stopPropagation();
+  }
+
+  public ActionsSideBarClick(event: MouseEvent) {
+    event.stopPropagation();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  public onResize(event) {
+    if (window.innerWidth < 768) {
+      this.isMobile = true;
+    }
+    else {
+      this.isMobile = false;
+    }
+  }
+
+
 
 }
